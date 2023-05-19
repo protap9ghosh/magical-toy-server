37b61accc4b3a6d464e09ba5f00ab9d2288ddd77
Protap Ghosh <protapg518@gmail.com>
2023-05-19 15:34:01 +0600

    get all toys

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ async function run() {
 
       const toyCollection = client.db('toyDB').collection('toy');
 
+      app.get('/toy', async (req, res) => {
+         const cursor = toyCollection.find();
+         const result = await cursor.toArray();
+         res.send(result);
+      })
+
       app.post('/toy', async(req, res) => {
          const newToy = req.body;
          console.log(newToy);
